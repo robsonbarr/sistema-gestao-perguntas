4689c3e2ba136d7a93de15af3048423c294b1649
Robson Barros <robsonbarros@rtecdevweb.com>
2022-06-04 19:38:48 -0300

    adicionado funcao de mudar titulo html da modal faltando ainda mudar os outros elementos necessarios

--- public/js/main.js
+++ public/js/main.js
@@ -3,12 +3,39 @@ import  Modal  from './modal.js'
 
 
 const modal = Modal()
+/*vou mudar os elementos html */
+
+const modalTitulo = document.querySelector('.modal-full h2')
+
+const modalDescription = document.querySelector('.modal-full p')
+
+const modalButton = document.querySelector('.modal-full button')
+
+
+
+//para baixo normal
+
 
 const checkButtons = document.querySelectorAll(".actions a.check")
 
 checkButtons.forEach(button => {
 
 button.addEventListener("click", event => {
+
+modalTitulo.innerHTML = "marcar como lida"
+  
+  modal.open()
+})
+
+
+})
+
+const delButtons = document.querySelectorAll(".actions a.del")
+
+delButtons.forEach(button => {
+
+button.addEventListener("click", event => {
+  modalTitulo.innerHTML = "exluir essa pergunta"
   //button.addEventListener("click",listener[, options]);
   modal.open()
 })
@@ -17,5 +44,5 @@ button.addEventListener("click", event => {
 })
 
 
-
-
+//1:45 tem que copiar as manhas de concatenaçao que há no codigo da jake para uma boa semantica de codigo 
+//pusa para copiar e só entao da continuidade
